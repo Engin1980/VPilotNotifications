d56767722c40a0e0b2239f7f3ea4751551223a08
Marek Vajgl <engin@seznam.cz>
2024-05-05 11:00:25 +0200

    Initial commit

---
+++ vPilotMessageAlert/MockBroker.cs
@@ -0,0 +1,17 @@
+﻿using RossCarlson.Vatsim.Vpilot.Plugins.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPilotMessageAlert
+{
+  public class MockBroker
+  {
+    public event Action<object?, NetworkConnectedEventArgs>? NetworkConnected;
+    public event Action<object?, EventArgs>? NetworkDisconnected;
+    public event Action<object?, RadioMessageReceivedEventArgs>? RadioMessageReceived;
+    public event Action<object?, SelcalAlertReceivedEventArgs>? SelcalAlertReceived;
+  }
+}
---
+++ vPilotMessageAlert/VPilotPlugin.cs
@@ -0,0 +1,40 @@
+﻿using RossCarlson.Vatsim.Vpilot.Plugins;
+
+namespace VPilotMessageAlert
+{
+  public class VPilotPlugin : RossCarlson.Vatsim.Vpilot.Plugins.IPlugin
+  {
+    public string Name => "VPilotMessageAlert";
+    private BrokerProxy? brokerProxy;
+
+    public void Initialize(IBroker broker)
+    {
+      this.brokerProxy = new(broker);
+
+      this.brokerProxy.NetworkConnected += Broker_NetworkConnected;
+      this.brokerProxy.NetworkDisconnected += Broker_NetworkDisconnected;
+      this.brokerProxy.RadioMessageReceived += Broker_RadioMessageReceived;
+      this.brokerProxy.SelcalAlertReceived += Broker_SelcalAlertReceived;
+    }
+
+    private void Broker_SelcalAlertReceived(object? sender, RossCarlson.Vatsim.Vpilot.Plugins.Events.SelcalAlertReceivedEventArgs e)
+    {
+      throw new NotImplementedException();
+    }
+
+    private void Broker_RadioMessageReceived(object? sender, RossCarlson.Vatsim.Vpilot.Plugins.Events.RadioMessageReceivedEventArgs e)
+    {
+      throw new NotImplementedException();
+    }
+
+    private void Broker_NetworkDisconnected(object? sender, EventArgs e)
+    {
+      throw new NotImplementedException();
+    }
+
+    private void Broker_NetworkConnected(object? sender, RossCarlson.Vatsim.Vpilot.Plugins.Events.NetworkConnectedEventArgs e)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
